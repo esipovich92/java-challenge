91b41832a23e05014198185a74c5acd479b28248
esipovich92 <esipovich92@gmail.com>
2018-04-05 22:37:14 +0400

    Initial commit

---
+++ src/com/esipovich/challenge/palindrome/Palindrome.java
@@ -0,0 +1,20 @@
+package com.esipovich.challenge.palindrome;
+
+/**
+ * @author Artem Esipovich 05.04.2018
+ *
+ * Create a function which checks if a given word is palindrome. Character case should be ignored
+ */
+
+public class Palindrome {
+    private static boolean isPalindrome(String word) {
+        StringBuilder wordToReverse = new StringBuilder(word);
+        wordToReverse.reverse();
+        return word.equalsIgnoreCase(wordToReverse.toString());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Palindrome.isPalindrome("Palindrom"));
+        System.out.println(Palindrome.isPalindrome("Kolok"));
+    }
+}
