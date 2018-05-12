c5265fe31b0cef25f36be68380768d725d69a106
esipovich92 <esipovich92@gmail.com>
2018-05-12 17:24:17 +0300

    Group anagrams

--- src/com/esipovich/challenge/group_anagrams/GroupAnagrams.java
+++ src/com/esipovich/challenge/group_anagrams/GroupAnagrams.java
@@ -1,7 +1,6 @@
 package com.esipovich.challenge.group_anagrams;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author Artem Esipovich 5/11/2018
@@ -20,10 +19,21 @@ public class GroupAnagrams {
 
     public static void main(String[] args) {
         List<List<String>> anagrams = groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"});
+        anagrams.forEach(System.out::println);
     }
 
     private static List<List<String>> groupAnagrams(String[] anagrams){
-        return new ArrayList<>();
+        Map<String, List<String>> tempMap = new HashMap<>();
+        for (String anagram : anagrams) {
+            char[] chars = anagram.toCharArray();
+            Arrays.sort(chars);
+            String key = String.valueOf(chars);
+            if (!tempMap.containsKey(key)) {
+                tempMap.put(key, new ArrayList<>());
+            }
+            tempMap.get(key).add(anagram);
+        }
+        return new ArrayList<>(tempMap.values());
     }
 
 }
