415976cb4e25188ed4ed5090bcb751b20a9e3274
esipovich92 <esipovich92@gmail.com>
2018-06-07 10:17:35 +0300

    Reverse String

---
+++ src/com/esipovich/challenge/reverse_string/ReverseString.java
@@ -0,0 +1,21 @@
+package com.esipovich.challenge.reverse_string;
+
+/**
+ * @author Artem Esipovich 6/7/2018
+ *
+ * Write a function that takes a string as input and returns the string reversed.
+ *
+ * Example:
+ * Given s = "hello", return "olleh".
+ */
+
+public class ReverseString {
+
+    public static void main(String[] args) {
+        System.out.println(reverseString("hello"));
+    }
+
+    public static String reverseString(String s) {
+        return new StringBuilder(s).reverse().toString();
+    }
+}
