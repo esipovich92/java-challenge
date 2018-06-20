db1fa038f8689230b8ad1f60ec1d0f9d133f2c18
esipovich92 <esipovich92@gmail.com>
2018-06-20 21:26:32 +0400

    Data manipulation

--- src/com/esipovich/challenge/data_manipulation/DataManipulation.java
+++ src/com/esipovich/challenge/data_manipulation/DataManipulation.java
@@ -1,6 +1,8 @@
 package com.esipovich.challenge.data_manipulation;
 
 
+import java.util.Random;
+
 /**
  * @author Artem Esipovich 20.06.2018
  *
@@ -19,5 +21,43 @@ package com.esipovich.challenge.data_manipulation;
 
 public class DataManipulation {
 
+    public String replaceLetters(String letters) {
+        StringBuilder letterifiedString = new StringBuilder(letters);
+        Random randomNumber = new Random();
+        for (int i = 0; i < letters.length(); i++){
+            if ('?' == letters.charAt(i)){
+                letterifiedString.setCharAt(i, (char) (randomNumber.nextInt(26) + 'a'));
+            } else {
+                letterifiedString.setCharAt(i, letters.charAt(i));
+            }
+        }
+        return letterifiedString.toString();
+    }
+
+    public String replaceNumbers(String numbers) {
+        StringBuilder numerifiedString = new StringBuilder();
+        Random randomNumber = new Random();
+        for (int i = 0; i < numbers.length(); i++){
+            if ('#' == numbers.charAt(i)){
+                numerifiedString.append(randomNumber.nextInt(10));
+            } else {
+                numerifiedString.append(numbers.charAt(i));
+            }
+        }
+        return numerifiedString.toString();
+    }
+
+    public String replaceAll(String line) {
+        StringBuilder bothifiedString = new StringBuilder(line);
+        Random randomNumber = new Random();
+        for (int i = 0; i < line.length(); i++){
+            if ('?' == line.charAt(i)){
+                bothifiedString.setCharAt(i, (char) (randomNumber.nextInt(26) + 'a'));
+            } else if ('#' == line.charAt(i)){
+                bothifiedString.setCharAt(i, Character.forDigit(randomNumber.nextInt(10), 10));
+            }
+        }
+        return bothifiedString.toString();
+    }
 
 }
