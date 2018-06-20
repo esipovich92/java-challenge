97bcde504e1b3627c0e84e5ccd5c3c7374b2d3ab
esipovich92 <esipovich92@gmail.com>
2018-06-20 21:26:13 +0400

    Data manipulation

---
+++ src/com/esipovich/challenge/data_manipulation/DataManipulation.java
@@ -0,0 +1,23 @@
+package com.esipovich.challenge.data_manipulation;
+
+
+/**
+ * @author Artem Esipovich 20.06.2018
+ *
+ * method replaceLetters(String letters):
+ *  - should replace '?' with a random letter from a to z
+ *  - do not replace another symbol
+ *
+ * method replaceNumbers(String numbers):
+ *  - should replace '#' with a random digit from 0 to 9
+ *  - do not replace another symbol
+ *
+ * method replace replaceAll(String line):
+ *  - should implement both
+ *
+ */
+
+public class DataManipulation {
+
+
+}
