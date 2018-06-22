340bb1c202970205efffa7eb3eb0859e417ecdd9
esipovich92@gmail.com <sunshineintherain7>
2018-06-22 15:06:29 +0300

    Number to console

---
+++ src/com/esipovich/challenge/number_to_console/NumberToConsole.java
@@ -0,0 +1,21 @@
+package com.esipovich.challenge.number_to_console;
+
+/**
+ * @author Artem Esipovich 6/22/2018
+ *
+ * Write a program, which will read a number from a console and print it in such way:
+ * input: 41072819
+ * output:
+ *    *    *    ***   *****  ***    ***    *   ****
+ *   **   **   *   *      * *   *  *   *  **  *   *
+ *  * *    *  *     *    *  *  *   *   *   *  *   *
+ * *  *    *  *     *   *     *     ***    *   ****
+ * ******  *  *     *  *     *     *   *   *      *
+ *    *    *   *   *  *     *      *   *   *      *
+ *    *   ***   ***   *     *****   ***   ***     *
+ *
+ */
+
+public class NumberToConsole {
+
+}
