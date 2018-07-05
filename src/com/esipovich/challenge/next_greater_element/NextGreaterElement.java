26d79418b5fe373a8341bc115648e47e07d17abe
esipovich92 <esipovich92@gmail.com>
2018-07-05 19:53:09 +0400

    Next Greater Element

---
+++ src/com/esipovich/challenge/next_greater_element/NextGreaterElement.java
@@ -0,0 +1,25 @@
+package com.esipovich.challenge.next_greater_element;
+
+/**
+ * @author Artem Esipovich 05.07.2018
+ *
+ * You are given two arrays (without duplicates) nums1 and nums2 where nums1’s elements are subset of nums2.
+ * Find all the next greater numbers for nums1's elements in the corresponding places of nums2.
+ *
+ * The Next Greater Number of a number x in nums1 is the first greater number to its right in nums2.
+ * If it does not exist, output -1 for this number.
+ *
+ * Input: nums1 = [4,1,2], nums2 = [1,3,4,2].
+ * Output: [-1,3,-1]
+ *
+ * Input: nums1 = [2,4], nums2 = [1,2,3,4].
+ * Output: [3,-1]
+ *
+ * Note:
+ * - All elements in nums1 and nums2 are unique.
+ * - The length of both nums1 and nums2 would not exceed 1000.
+ */
+
+public class NextGreaterElement {
+
+}
