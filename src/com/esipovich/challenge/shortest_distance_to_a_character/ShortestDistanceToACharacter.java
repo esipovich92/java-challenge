cf3b49c25106a628679dd76441ca795c8d33c8f4
esipovich92 <esipovich92@gmail.com>
2018-07-05 19:55:26 +0400

    Shortest distance to a character

--- src/com/esipovich/challenge/shortest_distance_to_a_character/ShortestDistanceToACharacter.java
+++ src/com/esipovich/challenge/shortest_distance_to_a_character/ShortestDistanceToACharacter.java
@@ -1,5 +1,9 @@
 package com.esipovich.challenge.shortest_distance_to_a_character;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author Artem Esipovich 05.07.2018
  *
@@ -17,4 +21,46 @@ package com.esipovich.challenge.shortest_distance_to_a_character;
  */
 
 public class ShortestDistanceToACharacter {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(shortestToChar("loveleetcode", 'e')));
+    }
+
+    private static int[] shortestToChar(String text, char symbol) {
+        int[] distanceArray = new int[text.length()];
+
+        List<Integer> indexes = findIndexes(text, symbol);
+
+        fillFirstDistance(distanceArray, indexes);
+
+        replaceWithShortestDistances(distanceArray, indexes);
+
+        return distanceArray;
+    }
+
+    private static List<Integer> findIndexes(String text, char symbol){
+        List<Integer> indexes = new ArrayList<>();
+        for (int i = 0; i < text.length(); i++){
+            if (text.charAt(i) == symbol){
+                indexes.add(i);
+            }
+        }
+        return indexes;
+    }
+
+    private static void fillFirstDistance(int[] distances, List<Integer> indexes){
+        for (int i = 0; i < distances.length; i++){
+            distances[i] = Math.abs(i - indexes.get(0));
+        }
+    }
+
+    private static void replaceWithShortestDistances(int[] distances, List<Integer> indexes){
+        for (int i = 1; i < indexes.size(); i++){
+            for (int j = 0; j < distances.length; j ++){
+                int currentDistance = Math.abs(j - indexes.get(i));
+                if (currentDistance < distances[j]){
+                    distances[j] = currentDistance;
+                }
+            }
+        }
+    }
 }
