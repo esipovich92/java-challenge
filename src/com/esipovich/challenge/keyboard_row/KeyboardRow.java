12c17e0bdb0ef8ba5412d74aaa83558dc6e3aee0
esipovich92 <esipovich92@gmail.com>
2018-07-06 21:45:26 +0400

    Keyboard Row

---
+++ src/com/esipovich/challenge/keyboard_row/KeyboardRow.java
@@ -0,0 +1,22 @@
+package com.esipovich.challenge.keyboard_row;
+
+/**
+ * @author Artem Esipovich 06.07.2018
+ *
+ * Given a List of words, return the words that can be typed using letters of alphabet on only one
+ * row's of American keyboard like the example below.
+ *
+ * First line: qwertyuiop
+ * Second line: asdfghjkl
+ * Third line: zxcvbnm
+ *
+ * Input: ["Hello", "Alaska", "Dad", "Peace"]
+ * Output: ["Alaska", "Dad"]
+ *
+ * Note:
+ * - You may use one character in the keyboard more than once.
+ * - You may assume the input string will only contain letters of alphabet.
+ */
+
+public class KeyboardRow {
+}
